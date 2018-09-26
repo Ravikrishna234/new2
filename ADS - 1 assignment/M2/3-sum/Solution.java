b584979acc61eb7277b59cf5be12d5c1df066b5a
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-26 21:09:00 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ ADS - 1 assignment/M2/3-sum/Solution.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+import java.util.Arrays;
+/**SUM.**/
+final class Solution {
+    /**
+     * solution.
+     */
+    private Solution() { }
+    /**
+     * @param args value
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        int size = s.nextInt();
+        int[] a = new int[size];
+        for (int i = 0; i < size; i++) {
+            a[i] = s.nextInt();
+        }
+        Arrays.sort(a);
+        boolean c;
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int low = j + 1;
+                int high = size - 1;
+                c = search(low, high, a, -(a[i] + a[j]));
+                if (c) {
+                    count++;
+                }
+
+            }
+        }
+        System.out.println(count);
+    }
+    /**
+     * @param low value
+     * @param high value
+     * @param a value
+     * @param search value
+     * @return count
+     */
+    public static boolean search(int low, int high, int[] a, int search) {
+                    int middle = 0;
+                    int count = 0;
+                    while (low <= high) {
+                    middle = (low + high) / 2;
+                    if (search == a[middle]) {
+                        return true;
+                    } else if (a[middle] > search) {
+                        high = middle - 1;
+                    } else {
+                        low = middle + 1;
+                    }
+                }
+                return false;
+            }
+        }
