9be86813e5266a69b75757c44def0c993416b8fb
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-28 20:46:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS - 1 assignment/M4/5.2 Josephus Problem/Solution.java
+++ ADS - 1 assignment/M4/5.2 Josephus Problem/Solution.java
@@ -1,20 +1,29 @@
-import java.util.*;
+import java.util.Scanner;
+/**JOSEPHSON.**/
 public class Solution {
-	public static void main(String[] args) {
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 */
+	private Solution() { }
+	/**
+	 * @param args value
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		int num = Integer.parseInt(s.nextLine());
 		int j = 0;
-		while(s.hasNextLine()) {
+		while (s.hasNextLine()) {
 			String[] tokens = s.nextLine().split(" ");
 			 int num1 = Integer.parseInt(tokens[0]);
 			 int num2 = Integer.parseInt(tokens[1]);
 			 String s2 = "";
 			 LinkedList link = new LinkedList();
-			 for(int i = 0; i < num1; i++) {
+			 for (int i = 0; i < num1; i++) {
 			 	link.Insertback(i);
 			 }
-			 while(!link.isEmpty()) {
-			 for(int i = 0; i < num2 - 1; i++) {
+			 while (!link.isEmpty()) {
+			 for (int i = 0; i < num2 - 1; i++) {
 			 	link.Insertback(link.Deletefront());
 			 }
 			 s2 += link.Deletefront() + " ";
