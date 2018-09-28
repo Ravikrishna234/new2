0c9bc4171a85d763da78f3d2d51bbb8f8b775bb8
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-28 16:58:23 +0530

    "check_style_score -> 1 of 5 passed. style score: 0/1 "

---
+++ ADS - 1 assignment/M4/5.1 Deque/LinkedList.java
@@ -0,0 +1,119 @@
+final class LinkedList {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Node head;
+    private Node tail;
+    private int size;
+    /**node.**/
+    class Node {
+        /**
+         * variable.
+         */
+        private int value;
+        /**
+         * variable.
+         */
+        private Node next;
+        /**
+         * variable.
+         */
+
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    LinkedList() {
+        head = tail;
+        head = null;
+        size = 0;
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @return value
+     */
+    public boolean isEmpty() {
+        if (head == null) {
+            return true;
+        }
+        return false;
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @return value
+     */
+    public void Deletefront() {
+        if (head!=null) {
+        head = head.next;
+        size--;
+    }
+    }
+    public void Deleteback() {
+        if (tail != null) {
+       Node temp = null;
+       Node popped = tail;
+       Node element = head;
+       while(element != tail) {
+            temp = temp.next;
+        }
+
+        tail = temp;
+        temp.next = null;
+        size--;
+    }
+    }
+    /**
+     * @param value value
+     */
+    public void Insertfront(final int value) {
+        if(head == null) {
+        //Node oldHead = head;
+        head = new Node();
+        head.value = value;
+        head.next = null;
+        tail = head;
+    } else {
+        Node oldHead = head;
+        head = new Node();
+        head.value = value;
+        head.next = oldHead;
+    }
+    size++;
+    }
+public void Insertback(final int value) {
+        if(tail == null) {
+        //Node temp = tail;
+            tail = new Node();
+            tail.value = value;
+            tail.next = null;
+        //temp.next = tail;
+            head = tail;
+        }   else {
+            Node temp = tail;
+            tail = new Node();
+            tail.value = value;
+            tail.next = null;
+            temp.next = tail;
+        }
+        size++;
+    }
+    public int getsize() {
+        return size;
+    }
+    public String displayAll() {
+        if(size!=0) {
+            String s = "[";
+            Node temp = head;
+            while(temp!=null){
+                s += temp.value + ", ";
+                temp = temp.next;
+            }
+            return s.substring(0,s.length()-2) + "]";
+        }
+        return "[]";
+    }
+}
