7bdd3198bcd017dfba2f1695610d492a56910875
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-09-28 20:54:03 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS - 1 assignment/M4/5.1 Deque/Solution.java
+++ ADS - 1 assignment/M4/5.1 Deque/Solution.java
@@ -1,47 +1,55 @@
-import java.util.*;
+import java.util.Scanner;
+/**DEQUEUE.**/
 public class Solution {
-    public static void main(String[] args) {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @param args value
+     */
+    public static void main(final String[] args) {
         Deque dequeue = new Deque();
         //LinkedList linklist = new LinkedList();
         Scanner s = new Scanner(System.in);
         String s1 = s.nextLine();
         int num = Integer.parseInt(s1);
         int j = 0;
-        while(j < num) {
+        while (j < num) {
         String[] tokens = s.nextLine().split(" ");
         //System.out.println(tokens[0] + "  " + tokens[1]);
-
-            switch(tokens[0]) {
+            switch (tokens[0]) {
                 case "pushLeft" :
-                                 dequeue.addFirst(Integer.parseInt(tokens[1]));
+                    dequeue.addFirst(Integer.parseInt(tokens[1]));
                                  //System.out.println("hello");
-                                 System.out.println(dequeue.display());
-                                 break;
+                    System.out.println(dequeue.display());
+                    break;
                 case "pushRight":
-                                 dequeue.addLast(Integer.parseInt(tokens[1]));
-                                 System.out.println(dequeue.display());
-                                 break;
+                    dequeue.addLast(Integer.parseInt(tokens[1]));
+                    System.out.println(dequeue.display());
+                    break;
                 case "popLeft":
-                                if(!dequeue.isempty()) {
-                                dequeue.removeFirst();
-                                System.out.println(dequeue.display());
-                            } else {
-                                System.out.println("Deck is empty");
+                    if(!dequeue.isempty()) {
+                        dequeue.removeFirst();
+                        System.out.println(dequeue.display());
+                        } else {
+                            System.out.println("Deck is empty");
                             }
                                 //System.out.println(linklist);
-                                break;
+                            break;
                 case "popRight":
-                                if(!dequeue.isempty()) {
-                                dequeue.removeLast();
-                                System.out.println(dequeue.display());
+                        if(!dequeue.isempty()) {
+                        dequeue.removeLast();
+                        System.out.println(dequeue.display());
                             } else {
-                                System.out.println("Deck is empty");
+                        System.out.println("Deck is empty");
                             }
                                 break;
                 case "size":
-                            int size = dequeue.size();
-                            System.out.println(size);
-                            break;
+                        int size = dequeue.size();
+                        System.out.println(size);
+                        break;
             }
             j++;
 
