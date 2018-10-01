aae052007f3b64b7b6eddccf8f1b0a8146f4e303
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-01 18:07:29 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- ADS - 1 assignment/M7/7.1 Steque/Solution.java
+++ ADS - 1 assignment/M7/7.1 Steque/Solution.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
-public class Solution {
-    public static void main(String[] args) {
+/**Steque.**/
+final class Solution {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @param args value
+     */
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int n = Integer.parseInt(s.nextLine());
         int i = 0;
-        while(i < n) {
+        while (i < n) {
             steque stq = new steque();
-            while(s.hasNextLine()) {
+            while (s.hasNextLine()) {
                 String[] tokens = s.nextLine().split(" ");
-                switch(tokens[0]) {
+                switch (tokens[0]) {
                     case "push" : stq.push(Integer.parseInt(tokens[1]));
                                   System.out.println(stq.displayAll());
                                   break;
                     case "pop" :stq.pop();
-                                if(stq.isEmpty()) {
+                                if (stq.isEmpty()) {
                                 System.out.println("Steque is empty.");
                                 } else {
                                  System.out.println(stq.displayAll());
@@ -22,8 +31,10 @@ public class Solution {
                     case "enqueue":stq.Insertback(Integer.parseInt(tokens[1]));
                                     System.out.println(stq.displayAll());
                                     break;
+                    default:
+                            break;
                 }
-                if(tokens[0].equals("")){
+                if (tokens[0].equals("")) {
                     System.out.println();
                     break;
                 }
