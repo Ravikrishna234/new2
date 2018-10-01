6d690631bf16bd13a5c14d0a9ed7c1c75e93005a
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-01 11:01:03 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS - 1 assignment/M7/7.1 Steque/Solution.java
+++ ADS - 1 assignment/M7/7.1 Steque/Solution.java
@@ -24,6 +24,7 @@ public class Solution {
                                     break;
                 }
                 if(tokens[0].equals("")){
+                    System.out.println();
                     break;
                 }
 
