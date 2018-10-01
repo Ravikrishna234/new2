723bf5052cbae2bed4319d6147c5164874907b8c
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-01 10:28:28 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS - 1 assignment/M7/7.1 Steque/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = Integer.parseInt(s.nextLine());
+        int i = 0;
+        while(i < n) {
+            steque stq = new steque();
+            while(s.hasNextLine()) {
+                String[] tokens = s.nextLine().split(" ");
+                switch(tokens[0]) {
+                    case "push" : stq.push(Integer.parseInt(tokens[1]));
+                                  System.out.println(stq.displayAll());
+                                  break;
+                    case "pop" : stq.pop();
+                                 System.out.println(stq.displayAll());
+                                 break;
+                    case "enqueue":stq.Insertback(Integer.parseInt(tokens[1]));
+                                    System.out.println(stq.displayAll());
+                                    break;
+                }
+
+            }
+            i++;
+        }
+    }
+}
