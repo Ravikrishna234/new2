86a51c07b0dc40048cf7923bd76b5e23ea4bfca3
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-02 18:10:13 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS - 1 assignment/M8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS - 1 assignment/M8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 /**SELECTIONSORT.**/
 final class Solution {
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 */
-	private Solution() { }
-	/**
-	 * @param args value.
-	 */
-	public static void main(final String[] args) {
-		Match m = new Match();
-		Scanner s = new Scanner(System.in);
-		while (s.hasNextLine()) {
-			String[] tokens = s.nextLine().split(",");
-			Team t = new Team(tokens[0],tokens[1],tokens[2],tokens[3]);
-			m.addteam(t);
-		}
-		m.selectionsort();
-		System.out.println(m);
-	}
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @param args value.
+     */
+    public static void main(final String[] args) {
+        Match m = new Match();
+        final int value = 1;
+        final int value1 = 0;
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextLine()) {
+            String[] tokens = s.nextLine().split(",");
+            Team t = new Team(tokens[value1],tokens[value],
+                tokens[value * 2],tokens[value * 3]);
+
+            m.addteam(t);
+        }
+        m.selectionsort();
+        System.out.println(m);
+    }
 }
