6c05d395b4659dcdca4f96e640ce121a771c4716
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-02 18:06:42 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS - 1 assignment/M8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS - 1 assignment/M8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
-
-class Solution {
-	public static void main(String[] args) {
+/**SELECTIONSORT.**/
+final class Solution {
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 */
+	private Solution() { }
+	/**
+	 * @param args value.
+	 */
+	public static void main(final String[] args) {
 		Match m = new Match();
 		Scanner s = new Scanner(System.in);
-		while(s.hasNextLine()) {
+		while (s.hasNextLine()) {
 			String[] tokens = s.nextLine().split(",");
 			Team t = new Team(tokens[0],tokens[1],tokens[2],tokens[3]);
 			m.addteam(t);
