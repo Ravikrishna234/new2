5bef9c5e6cd54d8684bd313ad8b1c877682eaf9a
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-03 19:12:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS - 1 assignment/M9/9.1 Merge Sort Improvements/Solution.java
+++ ADS - 1 assignment/M9/9.1 Merge Sort Improvements/Solution.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 /**MERGE.**/
-class Solution {
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 */
-	private Solution() { }
-	/**
-	 * @param args value
-	 * Time complexity is O(N).
-	 */
-	public static void main(final String[] args) {
-		Merge m = new Merge();
-		Scanner s = new Scanner(System.in);
-		String[] tokens = new String[50];
-		while (s.hasNextLine()) {
-			 tokens = s.nextLine().split(",");
-			 m.sort(tokens);
-			System.out.println(m.show(tokens));
-			System.out.println();
-		}
-	}
+final class Solution {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @param args value
+     * Time complexity is O(N).
+     */
+    public static void main(final String[] args) {
+        Merge m = new Merge();
+        Scanner s = new Scanner(System.in);
+        final int len = 50;
+        String[] tokens = new String[len];
+        while (s.hasNextLine()) {
+             tokens = s.nextLine().split(",");
+             m.sort(tokens);
+            System.out.println(m.show(tokens));
+            System.out.println();
+        }
+    }
 }
