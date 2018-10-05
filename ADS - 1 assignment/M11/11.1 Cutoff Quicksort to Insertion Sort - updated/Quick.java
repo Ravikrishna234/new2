dfab5cc3a8e8f46145f10bd0e180ba5fa42789d3
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-05 19:47:19 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 1 assignment/M11/11.1 Cutoff Quicksort to Insertion Sort - updated/Quick.java
+++ ADS - 1 assignment/M11/11.1 Cutoff Quicksort to Insertion Sort - updated/Quick.java
@@ -1,12 +1,12 @@
-import java.util.Collections;
-class Quick {
+final class Quick {
 	// Comparable[] arr;
 	Quick() {
 
 	}
 // time complexity for this method is O(N^2).
 // In first for loop and second loop it iterates through the size of array
-	public void insertion(Comparable[] arr, int low, int high) {
+	public void insertion(final Comparable[] arr,
+        final int low, final int high) {
 		// int i = low;
 		// int j = high;
 		for (int i = low; i <= high; i++) {
@@ -16,14 +16,15 @@ class Quick {
 		}
 	}
 // time complexity of this method is O(1). It swaps the elements only once.
-	public void swap(Comparable[] arr, int j, int min) {
+	public void swap(final Comparable[] arr,
+        final int j, final int min) {
 		Comparable temp = arr[j];
 	    arr[j] = arr[min];
 	    arr[min] = temp;
 	}
 // time complexity of this method is O(N). It iterates through
 // the array to print all the objects in array.
-	public String toString(Comparable[] arr) {
+	public String toString(final Comparable[] arr) {
 		// System.out.println("in print");
 		String str = "[";
 		for (int i = 0; i < arr.length - 1; i++) {
@@ -32,8 +33,9 @@ class Quick {
 		str += arr[arr.length - 1] + "]";
 		return str;
 	}
-// time complexity is O(N)
-	public void sort(Comparable[] arr, int low, int high, int cutOff)
+// time complexity is O(log(N))
+	public void sort(final Comparable[] arr, final int low,
+        final int high, final int cutOff)
     {
         if (high <= low + cutOff - 1) {
         	insertion(arr, low, high);
@@ -53,7 +55,7 @@ class Quick {
     	sort(array, 0, array.length - 1, cutOff);
         System.out.println(toString(array));
     }
-// time complexity is O(N)
+// time complexity is O(N^2)
     public int partition(Comparable[] arr, int low, int high) {
         int i = low;
         int j = high + 1;
