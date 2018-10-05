b483462cb0a1797889b8882a35bdfc5288f5be95
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-05 19:36:33 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/M11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
+++ ADS - 1 assignment/M11/11.1 Cutoff Quicksort to Insertion Sort - updated/Solution.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
+/**QUICKSORT.**/
 class Solution{
-	public static void main(String[] args) {
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 */
+	private Solution() { }
+	/**
+	 * @param args value
+	 */
+	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		Quick quick = new Quick();
 		int n = Integer.parseInt(s.nextLine());
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			int cutoff = Integer.parseInt(s.nextLine());
 			String[] tokens = s.nextLine().split(" ");
-			if(tokens[0].equals("")) {
+			if (tokens[0].equals("")) {
 				System.out.println("[]");
 			} else {
-			quick.sort(tokens,cutoff);
+			quick.sort(tokens, cutoff);
 		}
 
 		}
