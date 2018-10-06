c233355f21d6d1413ccbfee83bb0e95e18b39b47
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-06 12:26:55 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS - 1 assignment/M12/Assignment-new/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		int n = Integer.parseInt(s.nextLine());
+		int vacancy = Integer.parseInt(s.nextLine());
+		int uvacancy = Integer.parseInt(s.nextLine());
+		int bc = Integer.parseInt(s.nextLine());
+		int sc = Integer.parseInt(s.nextLine());
+		int st = Integer.parseInt(s.nextLine());
+		store sto = new store();
+		student stud = new student(vacancy, uvacancy, bc, sc, st);
+		int i = 0;
+		while(i < n) {
+			String[] tokens = s.nextLine().split(",");
+			stud = new student(tokens[0],tokens[1],Integer.parseInt(tokens[2]),
+				Integer.parseInt(tokens[3]),Integer.parseInt(tokens[4]),
+				Integer.parseInt(tokens[5]),tokens[6]);
+			sto.addStudent(stud);
+			i++;
+		}
+		sto.selectionsort();
+		System.out.println(sto);
+		System.out.print("\n");
+		sto.selectionsort1();
+		System.out.println(sto.toString1(vacancy));
+	}
+}
---
+++ ADS - 1 assignment/M12/Assignment-new/student.java
@@ -0,0 +1,195 @@
+import java.util.Date;
+import java.util.Locale;
+class student{
+	String name;
+	String dob;
+	int sub1;
+	int sub2;
+	int sub3;
+	int total;
+	String reservation;
+	int vacancy;
+	int unreserved;
+	int bc;
+	int sc;
+	int st;
+	student(int vacancy,int unre,int bc,int sc,int st) {
+		this.vacancy = vacancy;
+		this.unreserved = unre;
+		this.bc = bc;
+		this.sc = sc;
+		this.st = st;
+	}
+	public int getvacancy() {
+		return this.vacancy;
+	}
+	public int getst() {
+		return this.st;
+	}
+	public int getsc() {
+		return this.sc;
+	}
+	public int getbc() {
+		return this.bc;
+	}
+	public int getunreserve() {
+		return this.unreserved;
+	}
+	//Date start = new SimpleDateFormat("yyyy/MM/dd", Locale.ENGLISH)
+                    //.parse(startDate);
+	student(String name,String dob,int sub1,int sub2,int sub3,int total,String res) {
+		this.name = name;
+		this.dob = dob;
+		this.sub1 = sub1;
+		this.sub2 = sub2;
+		this.sub3 = sub3;
+		this.total = total;
+		this.reservation = res;
+	}
+	public String getname() {
+		return this.name;
+	}
+	public String getdob() {
+		return this.dob;
+	}
+	public int get1() {
+		return this.sub1;
+	}
+	public int get2() {
+		return this.sub2;
+	}
+	public int get3() {
+		return this.sub3;
+	}
+	public int gettotal() {
+		return this.total;
+	}
+	public String getreserve() {
+		return this.reservation;
+	}
+	public int compareTo(student that) {
+		if(this.gettotal() > that.gettotal()) {
+			return 1;
+		}
+		else if(this.gettotal() < that.gettotal()) {
+			return -1;
+		 }
+		// else {
+		// if(this.getlosse() < that.getlosse()) {
+		// 	return 1;
+		// }
+		// else if(this.getlosse() > that.getlosse()) {
+		// 	return -1;
+		// } else {
+		// if(this.getdraws() < that.getdraws()) {
+		// 	return -1;
+		// } else if(this.getdraws() > that.getdraws()) {
+		// 	return 1;
+		// }
+			else {
+		return 0;
+	}
+}
+	public int compare(student that) {
+		if(this.gettotal() > that.gettotal()) {
+			return 1;
+		}
+		 else if(this.gettotal() < that.gettotal()) {
+		 	return -1;
+		 }
+		 else if(this.get3() > that.get3()) {
+		 	return 1;
+		 } else if(this.get3() < that.get3()) {
+		 	return -1;
+		 } else if(this.get2() > that.get2()) {
+		 	return 1;
+		 } else if(this.get2() < that.get2()) {
+		 	return - 1;
+		 } else if(this.getdob().compareTo(that.getdob()) > 0) {
+		 	return 1;
+		 } else if(this.getdob().compareTo(that.getdob()) < 0) {
+		 	return -1;
+		 }
+			else {
+		return 0;
+	}
+}
+
+}
+class store {
+	student[] student;
+	int size;
+	store() {
+		student = new student[50];
+		size = 0;
+	}
+	public void addStudent(student s) {
+		student[size++] = s;
+	}
+	public void selectionsort() {
+		int loc;
+		for(int i = 0; i < size - 1; i++) {
+			loc = i;
+			for(int j = i + 1; j < size; j++) {
+				if((student[j].compareTo(student[loc])) >= 0) {
+					loc = j;
+				}
+			}
+			exchange(student,i,loc);
+		}
+	}
+	/**
+	 * time complexity is O(1).
+	 */
+	public void exchange(student[] team,int i,int loc) {
+	student temp = student[loc];
+	student[loc] = student[i];
+	student[i] = temp;
+			}
+	public void selectionsort1() {
+		int loc;
+		for(int i = 0; i < size - 1; i++) {
+			loc = i;
+			for(int j = i + 1; j < size; j++) {
+				if((student[j].compare(student[loc])) >= 0) {
+					loc = j;
+				}
+			}
+			exchange1(student,i,loc);
+		}
+	}
+	/**
+	 * time complexity is O(1).
+	 */
+	public void exchange1(student[] team1,int i,int loc) {
+	student temp = student[loc];
+	student[loc] = student[i];
+	student[i] = temp;
+			}
+	public String toString() {
+		String s = "";
+		for(int i = 0; i < size - 1; i++) {
+			s += student[i].getname() + "," + student[i].gettotal()
+			+ "," +student[i].getreserve();
+			s += "\n";
+		}
+		s += student[size - 1].getname() + "," + student[size - 1].gettotal()
+		+ "," +student[size - 1].getreserve();
+			return s;
+
+
+	}
+	public String toString1(int vacancy) {
+		String s = "";
+		for(int i = 0; i < vacancy - 1; i++) {
+			s += student[i].getname() + "," + student[i].gettotal()
+			+ "," +student[i].getreserve();
+			s += "\n";
+		}
+		s += student[vacancy - 1].getname() + "," + student[vacancy - 1].gettotal()
+		+ "," +student[vacancy - 1].getreserve();
+			return s;
+
+
+	}
+}
