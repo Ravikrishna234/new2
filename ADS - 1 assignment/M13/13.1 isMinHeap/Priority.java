815ca8ebfbae1d5dadf3183817ee302807cb0c45
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-08 19:00:02 +0530

    "ads1_m13_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/M13/13.1 isMinHeap/Priority.java
+++ ADS - 1 assignment/M13/13.1 isMinHeap/Priority.java
@@ -1,33 +1,34 @@
 import java.util.Arrays;
-class Priority {
-	Comparable[] array;
+class Priority<E extends Comparable<E>> {
+	E[] array;
 	int size;
-	Priority() {
-		array = new Comparable[20];
-		size = 0;
+	Priority(final E[] a) {
+		this.array = a;
 	}
-	public boolean addAll(Comparable[] arr) {
-		for(int i = 0; i < arr.length; i++) {
-		add(arr[i]);
-	}
-	if(isMinHeap(1)) {
-		return true;
-	} else {
+	public boolean isminHeap() {
+		if(isMinHeap(1)) {
+			return true;
+		  } else {
 		return false;
 	}
 	}
-	public void add(Comparable t) {
-		if(size == array.length - 1) {
-			resize();
-		}
-		array[++size] = t;
-	}
+	// public void addAll(E[] arr) {
+	// 	for(int i = 0; i < arr.length; i++) {
+	// 	add(arr[i]);
+	// }
+	// }
+	// public void add(E t) {
+	// 	if(size == array.length - 1) {
+	// 		resize();
+	// 	}
+	// 	array[++size] = t;
+	// }
 	public void resize() {
 		array = Arrays.copyOf(array,array.length * 2);
 	}
 	 public boolean isMinHeap(int k) {
-        if (k > size) {
-        	return true;
+        if (k < size) {
+        	return false;
         }
 
         if (2 * k <= size && greater(k, 2 * k)) {
@@ -40,5 +41,12 @@ class Priority {
     }
     public boolean greater(int i, int j) {
     	return (array[i].compareTo(array[j])) > 0;
+    }
+    public int compareTo(E that) {
+    	if(this.compareTo(that) > 0) {
+    		return 1;
+    	} else if(this.compareTo(that) < 0)
+    return -1;
+    	return 0;
     }
 }
