bc7354c68c149a5f9a5fdb1848a0e1da38a5f86d
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-08 19:38:58 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/M13/13.1 isMinHeap/Solution.java
+++ ADS - 1 assignment/M13/13.1 isMinHeap/Solution.java
@@ -15,51 +15,51 @@ class Solution {
 		int n = Integer.parseInt(s.nextLine());
 		switch(s1) {
 			case "String":
-				while (n > 0) {
-					String[] tokens = s.nextLine().split(",");
-					Priority<String> m = new Priority<String>(tokens);
-					System.out.println(m.isminHeap());
-					n--;
+			while (n > 0) {
+			String[] tokens = s.nextLine().split(",");
+			Priority<String> m = new Priority<String>(tokens);
+			System.out.println(m.isminHeap());
+			n--;
 				}
 				break;
 			case "Integer":
-				while (n > 0) {
-					String[] tokens = s.nextLine().split(",");
-               		Integer[] intTokens = new Integer[tokens.length];
-               		for (int i = 0; i < tokens.length;i++) {
-               			intTokens[i] = Integer.parseInt(tokens[i]);
-               		}
-               		Priority<Integer> m = new Priority<Integer>(intTokens);
-               		System.out.println(m.isminHeap());
-               		n--;
+			while (n > 0) {
+			String[] tokens = s.nextLine().split(",");
+            Integer[] intTokens = new Integer[tokens.length];
+            for (int i = 0; i < tokens.length;i++) {
+               	intTokens[i] = Integer.parseInt(tokens[i]);
+               }
+            Priority<Integer> m = new Priority<Integer>(intTokens);
+            System.out.println(m.isminHeap());
+            n--;
 				}
 				break;
 			case "Double":
-				while (n > 0) {
-				String[] tokens = s.nextLine().split(",");
-               		Double[] idTokens = new Double[tokens.length];
-               		for (int i = 0; i < tokens.length;i++) {
-               			idTokens[i] = Double.parseDouble(tokens[i]);
-               		}
-               		Priority<Double> m = new Priority<Double>(idTokens);
-               		System.out.println(m.isminHeap());
-               		n--;
+			while (n > 0) {
+			String[] tokens = s.nextLine().split(",");
+            Double[] idTokens = new Double[tokens.length];
+            for (int i = 0; i < tokens.length;i++) {
+               	idTokens[i] = Double.parseDouble(tokens[i]);
+           	}
+            Priority<Double> m = new Priority<Double>(idTokens);
+            System.out.println(m.isminHeap());
+            n--;
 				}
 		break;
 		case "Float":
-			while (n > 0) {
-			String[] tokens = s.nextLine().split(",");
-			if(tokens[0].equals("")) {
-				System.out.println("false");
-				break;
-			} else {
-				Float[] ftokens = new Float[tokens.length];
-				for (int i = 0; i < tokens.length; i++) {
-					ftokens[i] = Float.parseFloat(tokens[i]);
-				}
-				Priority<Float> m = new Priority<Float>(ftokens);
-				System.out.println(m.isminHeap());
-				n--;
+		while (n > 0) {
+		String[] tokens = s.nextLine().split(",");
+		if(tokens[0].equals("")) {
+			System.out.println("false");
+			break;
+		} else {
+		Float[] ftokens = new Float[tokens.length];
+		for (int i = 0; i < tokens.length; i++) {
+				ftokens[i] = Float.parseFloat(tokens[i]);
+			}
+		Priority<Float> m = new Priority<Float>(ftokens);
+		System.out.println(m.isminHeap());
+		n--;
 			}
 		}
 		break;
