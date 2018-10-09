89266d254594e8acdaf8f598bb365dd2c0e60c5e
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-09 22:16:37 +0530

    "ads1_m14_a2: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/M14/14.2 student reservation system/Solution.java
+++ ADS - 1 assignment/M14/14.2 student reservation system/Solution.java
@@ -10,10 +10,10 @@ class Solution {
          * @param args value
          * Time Complexity is O(N).
          */
-	public static void main(final String[] args) {
-	Scanner s = new Scanner(System.in);
-	store sto = new store();
-	int n = Integer.parseInt(s.nextLine());
+    public static void main(final String[] args) {
+    Scanner s = new Scanner(System.in);
+    store sto = new store();
+    int n = Integer.parseInt(s.nextLine());
         int vacancies = Integer.parseInt(s.nextLine());
         int open = Integer.parseInt(s.nextLine());
         int bc = Integer.parseInt(s.nextLine());
@@ -22,18 +22,18 @@ class Solution {
         int i = 0;
         while(i < n) {
                 /**STUDENT.**/
-        	String[] tokens = s.nextLine().split(",");
-        	student stud = new student(tokens[0],tokens[1],Integer.parseInt(tokens[2]),
-        		Integer.parseInt(tokens[3]),
-        		Integer.parseInt(tokens[4]),Integer.parseInt(tokens[5]),
-        			tokens[6]);
-        	sto.addStudent(stud);
-        	i++;
-        }
-        sto.sort();
-        System.out.println(sto.toString());
-        System.out.println("\n" +sto.toString1(open));
-        sto.getr(open,n,bc,sc,st);
-	}
+            String[] tokens = s.nextLine().split(",");
+        //     //student stud = new student(tokens[0],tokens[1],Integer.parseInt(tokens[2]),
+        //         //Integer.parseInt(tokens[3]),
+        //         //Integer.parseInt(tokens[4]),Integer.parseInt(tokens[5]),
+        //             tokens[6]);
+        //     sto.addStudent(stud);
+        //     i++;
+         }
+        // sto.sort();
+        // System.out.println(sto.toString());
+        // System.out.println("\n" +sto.toString1(open));
+        // sto.getr(open,n,bc,sc,st);
+    }
 }
 
