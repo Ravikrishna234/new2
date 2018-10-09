758232f02c2e182806a41f69cdb873f8acfc62df
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-09 10:07:48 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 1 assignment/M13/13.1 isMinHeap/Priority.java
+++ ADS - 1 assignment/M13/13.1 isMinHeap/Priority.java
@@ -5,6 +5,11 @@ class Priority<E extends Comparable<E>> {
 	Priority(final E[] a) {
 		this.array = a;
 	}
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * Time complexity is O(log(N))
+	 */
 	public boolean isminHeap() {
 		for(int i = 1; i < array.length; i++) {
 			if(2 * i < array.length && greater(2 * i, i)) {
@@ -15,6 +20,11 @@ class Priority<E extends Comparable<E>> {
 		}
 		return true;
 	}
+	/**
+	 * @brief [brief description]
+	 * @details [long description]
+	 * Time complexity is O(1)
+	 */
     public boolean greater(int i, int j) {
     	return array[i].compareTo(array[j]) < 0;
     }
