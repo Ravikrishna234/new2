fe05d59d19251a4cffdef4e6dd10f7ce69d862db
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-10 17:17:02 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/M14/14.1 Dynamic Median Finding/Solution.java
+++ ADS - 1 assignment/M14/14.1 Dynamic Median Finding/Solution.java
@@ -15,30 +15,30 @@ final class Solution {
 		Minpq<Float> min = new Minpq<Float>(n);
 		Maxpq<Float> max = new Maxpq<Float>(n);
 		float median = 0.0f;
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			float f = Float.parseFloat(s.nextLine());
-			if(f > median) {
+			if (f > median) {
 				min.insert(f);
 			} else {
 				max.insert(f);
 			}
-			if(min.size() - max.size() > 1) {
+			if (min.size() - max.size() > 1) {
 				float f1 = min.delMin();
 				max.insert(f1);
 			}
-			if(max.size() - min.size() > 1) {
+			if (max.size() - min.size() > 1) {
 				float f2 = max.delMax();
 				min.insert(f2);
 			}
-			if(min.size() == max.size()) {
+			if (min.size() == max.size()) {
 				median = (min.min() + max.max())/2;
 				System.out.println(median);
 			}
-			if(min.size() > max.size()) {
+			if (min.size() > max.size()) {
 				median = min.min();
 				System.out.println(median);
 			}
-			if(max.size() > min.size()) {
+			if (max.size() > min.size()) {
 				median = max.max();
 				System.out.println(median);
 			}
