83eeedea6baf97a0090a4764e89d5ca2cdf6f3cd
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-22 15:43:38 +0530

    "ads1_m19_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS - 1 assignment/M19/19.1 BST/Solution.java
+++ ADS - 1 assignment/M19/19.1 BST/Solution.java
@@ -19,35 +19,36 @@ final class Solution {
      String[] tokens = s.nextLine().split(",");
      switch (tokens[0]) {
         case "put":
-        			b = new Book(tokens[1], tokens[1 + 1],
-        			Float.parseFloat(tokens[1 + 1 + 1]));
+                    b = new Book(tokens[1], tokens[1 + 1],
+                    Float.parseFloat(tokens[1 + 1 + 1]));
                     bst.put(b,
                     Integer.parseInt(tokens[1 + 1 + 1 + 1]));
                     break;
         case "get":
-        			b = new Book(tokens[1], tokens[1 + 1],
-        			Float.parseFloat(tokens[1 + 1 + 1]));
+                    b = new Book(tokens[1], tokens[1 + 1],
+                    Float.parseFloat(tokens[1 + 1 + 1]));
                     System.out.println(bst.get(b));
                     break;
         case "max":
-        			System.out.println(bst.max());
-        			break;
+                    System.out.println(bst.max());
+                    break;
         case "min":
-        			System.out.println(bst.min());
-        			break;
+                    System.out.println(bst.min());
+                    break;
         case "floor":
-        			 b = new Book(tokens[1], tokens[1 + 1],
-       				Float.parseFloat(tokens[1 + 1 + 1]));
-        			System.out.println(bst.floor(b));
-        			break;
+                     b = new Book(tokens[1], tokens[1 + 1],
+                    Float.parseFloat(tokens[1 + 1 + 1]));
+                    System.out.println(bst.floor(b));
+                    break;
         case "ceiling":
-        			b = new Book(tokens[1], tokens[1 + 1],
-        			Float.parseFloat(tokens[1 + 1 + 1]));
-        			System.out.println(bst.ceiling(b));
-        			break;
+                    b = new Book(tokens[1], tokens[1 + 1],
+                    Float.parseFloat(tokens[1 + 1 + 1]));
+                    System.out.println(bst.ceiling(b));
+                    break;
         case "select":
-        			System.out.println(bst.select(Integer.parseInt(tokens[1])));
-        			break;
+                    System.out.println(bst.select(
+                        Integer.parseInt(tokens[1])));
+                    break;
 
         default:
                 break;
