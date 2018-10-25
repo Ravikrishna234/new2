ebfd290317c3c44b4373001b1518198f4cacf542
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-25 19:19:18 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/M22/22.1 Ransom Note/Solution.java
+++ ADS - 1 assignment/M22/22.1 Ransom Note/Solution.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 /**HASHTABLE.**/
 final class Solution {
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
     private Solution() { }
     /**
      * @brief [brief description]
@@ -32,7 +36,7 @@ final class Solution {
                 a[size++] = value;
             }
         }
-        int c = 0;
+         int c = 0;
         for (int i = 0; i < a.length; i++) {
             if (a[i] < 0) {
                 c = 0;
