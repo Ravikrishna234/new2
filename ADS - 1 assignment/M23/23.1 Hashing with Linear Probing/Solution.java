e71380ce6dc13056d67ac0cf3dc6cff19924947f
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-26 18:14:36 +0530

    "ads1_m23_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/M23/23.1 Hashing with Linear Probing/Solution.java
+++ ADS - 1 assignment/M23/23.1 Hashing with Linear Probing/Solution.java
@@ -1,23 +1,34 @@
-import java.util.*;
+import java.util.Scanner;
+/**LINEARPROBING.**/
 class Solution {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		LinearProbingHashST linearprob = new LinearProbingHashST();
-		int n = Integer.parseInt(s.nextLine());
-		for(int i = 0; i < n;i++) {
-			String[] tokens = s.nextLine().split(" ");
-			switch(tokens[0]) {
-				case "put": linearprob.put(tokens[1],Integer.parseInt(tokens[2]));
-							break;
-				case "get": System.out.println(linearprob.get(tokens[1]));
-							break;
-				case "delete":linearprob.delete(tokens[1]);
-							break;
-				case "display": linearprob.display();
-							break;
-				default:
-						break;
-			}
-		}
-	}
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     */
+    private Solution() { }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * @param args value
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        LinearProbingHashST linearprob = new LinearProbingHashST();
+        int n = Integer.parseInt(s.nextLine());
+        for (int i = 0; i < n;i++) {
+            String[] tokens = s.nextLine().split(" ");
+            switch (tokens[0]) {
+                case "put": linearprob.put(tokens[1],Integer.parseInt(tokens[2]));
+                            break;
+                case "get": System.out.println(linearprob.get(tokens[1]));
+                            break;
+                case "delete":linearprob.delete(tokens[1]);
+                            break;
+                case "display": linearprob.display();
+                            break;
+                default:
+                        break;
+            }
+        }
+    }
 }
