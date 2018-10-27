c15b005d86fbc72a539fa5be6bc55a834d194a64
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-27 12:25:08 +0530

    "m24_a1_1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS - 1 assignment/m24/1/Solution.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		// LinearProbingHashST hashing = new LinearProbingHashST();
+		LinearProbingHashST hashing = new LinearProbingHashST();
+		int n = Integer.parseInt(s.nextLine());
+		for(int i = 0; i < n; i++) {
+		String[] tokens = s.nextLine().split(",");
+		hashing.put(tokens[0], new Student(tokens[1],Double.parseDouble(tokens[2])));
+		//System.out.println("hai");
+
+	 }
+	 int n1 = Integer.parseInt(s.nextLine());
+	 //System.out.println("after loop");
+	 int i = 0;
+	 while(i < n1) {
+	 String[] tokens1 = s.nextLine().split(" ");
+	 //System.out.println(tokens1[0]);
+	 if(tokens1[0].equals("get")) {
+	 	//System.out.println("haiii");
+	 	if(Integer.parseInt(tokens1[2]) == 1) {
+	 		String a = hashing.get((tokens1[1]),Integer.parseInt(tokens1[2]));
+	 		String[] b = a.split(" ");
+	 		System.out.println(b[0]);
+	 } else {
+	 	String c = hashing.get((tokens1[1]),Integer.parseInt(tokens1[2]));
+	 	String[] d = c.split(" ");
+	 	System.out.println(d[1]);
+	 }
+
+
+	 }
+	 i++;
+	}
+	}
+}
---
+++ ADS - 1 assignment/m24/1/Student.java
@@ -0,0 +1,23 @@
+class Student{
+	int rollno;
+	String name;
+	Double marks;
+	Student(String name1, Double marks1){
+		//this.rollno =roll;
+		this.name = name1;
+		this.marks = marks1;
+	}
+	// public int getrollno() {
+	// 	return this.rollno;
+	// }
+	public String getname() {
+		return this.name;
+	}
+	public Double getmarks1() {
+		return this.marks;
+	}
+	public String toString() {
+		return this.getname() + " " + this.getmarks1();
+	}
+
+}
