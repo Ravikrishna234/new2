6e0793c8c71532fba1fa37e9dd67e3cfbcdb925f
Ravikrishna234 <ravikrishnadontula23@msitprogram.net>
2018-10-27 13:10:29 +0530

    "m24_a1_1: 3/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS - 1 assignment/m24/1/Student.java
+++ ADS - 1 assignment/m24/1/Student.java
@@ -1,43 +1,59 @@
 /**studenttable.**/
 final class Student {
-	int rollno;
-	String name;
-	Double marks;
-	Student(String name1, Double marks1){
-		//this.rollno =roll;
-		this.name = name1;
-		this.marks = marks1;
-	}
-	// public int getrollno() {
-	// 	return this.rollno;
-	// }
+    /**
+     * variable;
+     */
+    private int rollno;
+    /**
+     * variable;
+     */
+    private String name;
+    /**
+     * variable;
+     */
+    private Double marks;
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     *
+     * @param name1 value
+     * @param marks1 value
+     */
+    Student(final String name1, final Double marks1) {
+        //this.rollno =roll;
+        this.name = name1;
+        this.marks = marks1;
+    }
+    // public int getrollno() {
+    //  return this.rollno;
+    // }
 
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * Time complexity is O(1)
-	 * @return [description]
-	 */
-	public String getname() {
-		return this.name;
-	}
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * Time complexity is O(1)
-	 * @return [description]
-	 */
-	public Double getmarks1() {
-		return this.marks;
-	}
-	/**
-	 * @brief [brief description]
-	 * @details [long description]
-	 * Time complexity is O(1)
-	 * @return [description]
-	 */
-	public String toString() {
-		return this.getname() + "==" + this.getmarks1();
-	}
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * Time complexity is O(1)
+     * @return [description]
+     */
+    public String getname() {
+        return this.name;
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * Time complexity is O(1)
+     * @return [description]
+     */
+    public Double getmarks1() {
+        return this.marks;
+    }
+    /**
+     * @brief [brief description]
+     * @details [long description]
+     * Time complexity is O(1)
+     * @return [description]
+     */
+    public String toString() {
+        return this.getname() + "==" + this.getmarks1();
+    }
 
 }
